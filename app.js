d0cb07195ca5b31825439af6f32a104c9bc19cb6
AakashKathait <92093691+AakashKathait@users.noreply.github.com>
2022-03-14 20:24:47 -0700

    update

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.get('/', function(req, res){
     res.sendfile('/index.html');
 });
 
-// app.listen(8080);
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
