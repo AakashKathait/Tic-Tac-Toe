1389a13d50bf3dc83f69b029cef5089ead043877
AakashKathait <92093691+AakashKathait@users.noreply.github.com>
2022-03-14 20:11:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const express = require('express');
-const app = express();
+// grab express
+var express = require("express");
 
-app.listen(3000, () => {
-    console.log('listening...')
-})
+// create an express app
+var app = express();
+
+// CONFIGURE THE APP
+// ==================================================
+// tell node where to look for site resources
+app.use('/', express.static(__dirname + '/'));
+
+// http://localhost:8080/
+app.get('/', function(req, res){
+    res.sendfile('/index.html');
+});
+
+app.listen(8080);
+console.log('Server has started');
